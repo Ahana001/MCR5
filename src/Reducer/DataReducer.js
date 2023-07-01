13f91ca852a38f4d84f03488fcec1cf39e218f93
Ahana001 <Ankita-Thakkar>
2023-07-01 18:44:05 +0530

    initial set up

---
+++ src/Reducer/DataReducer.js
@@ -0,0 +1,11 @@
+export const ActionTypes = {};
+
+export const initialState = {};
+
+export function DataReducer(state, action) {
+  let result;
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+  }
+  return result;
+}
